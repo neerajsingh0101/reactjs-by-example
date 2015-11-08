725ff25761582853c1b67759fe209b54b44e9eb9
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-11-08 17:13:18 +0530

    Use inline bootstrap

--- chapter7/src/App.jsx
+++ chapter7/src/App.jsx
@@ -1,7 +1,7 @@
 require("jquery");
-require("bootstrap");
-require("bootstrap-webpack");
-require("font-awesome-webpack");
+// require("bootstrap");
+// require("bootstrap-webpack");
+// require("font-awesome-webpack");
 require('btoa');
 
 import React from 'react';
