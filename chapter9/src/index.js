4f01f0d9fd86d49432ae0e1016bead233df72bb6
Vipul A M <vipulnsward@gmail.com>
2015-12-19 12:39:33 +0530

    [Chapter 9] - Moved around components to respective files - Added backbone collection to manage cat images

--- chapter9/src/index.js
+++ chapter9/src/index.js
@@ -2,104 +2,41 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createHistory, useBasename } from 'history'
 import { Router, Route, IndexRoute, Link } from 'react-router'
+import Backbone from 'backbone';
+import Modal from './Modal'
+import App from './App'
 
 const history = useBasename(createHistory)({
   basename: '/pinterest'
-})
+});
 
-const PICTURES = [
-  { id: 0, src: 'http://placekitten.com/601/601' },
-  { id: 1, src: 'http://placekitten.com/610/610' },
-  { id: 2, src: 'http://placekitten.com/620/620' }
-]
-
-const Modal = React.createClass({
-  styles: {
-    position: 'fixed',
-    top: '20%',
-    right: '20%',
-    bottom: '20%',
-    left: '20%',
-    padding: 20,
-    boxShadow: '0px 0px 150px 130px rgba(0, 0, 0, 0.5)',
-    overflow: 'auto',
-    background: '#fff'
-  },
-
-  render() {
-    return (
-      <div style={this.styles}>
-        <p><Link to={this.props.returnTo}>Back</Link></p>
-        {this.props.children}
-      </div>
-    )
+const PictureModel = Backbone.Model.extend({
+  defaults : {
+    src  : 'http://lorempixel.com/601/600/cats/'
   }
-})
+});
 
-const App = React.createClass({
+const Cats = new Backbone.Collection;
+Cats.add(new PictureModel({src: "http://lorempixel.com/601/600/cats/"}));
+Cats.add(new PictureModel({src: "http://lorempixel.com/602/600/cats/"}));
+Cats.add(new PictureModel({src: "http://lorempixel.com/603/600/cats/"}));
+Cats.add(new PictureModel({src: "http://lorempixel.com/604/600/cats/"}));
+Cats.add(new PictureModel({src: "http://lorempixel.com/605/600/cats/"}));
+Cats.add(new PictureModel({src: "http://lorempixel.com/606/600/cats/"}));
 
-  componentWillReceiveProps(nextProps) {
-    // if we changed routes...
-    if ((
-        nextProps.location.key !== this.props.location.key &&
-        nextProps.location.state &&
-        nextProps.location.state.modal
-      )) {
-      // save the old children (just like animation)
-      this.previousChildren = this.props.children
-    }
-  },
+Cats.forEach( (cat) => {
+console.log(cat.cid);
+  console.log(cat.get('src'));
+});
 
-  render() {
-    let { location } = this.props
-
-    let isModal = (
-      location.state &&
-      location.state.modal &&
-      this.previousChildren
-    )
-
-    return (
-      <div>
-        <h1>Pinterest Style Routes</h1>
-
-        <div>
-          {isModal ?
-            this.previousChildren :
-            this.props.children
-          }
-
-          {isModal && (
-            <Modal isOpen={true} returnTo={location.state.returnTo}>
-              {this.props.children}
-            </Modal>
-          )}
-        </div>
-      </div>
-    )
-  }
-})
-
-const Index = React.createClass({
+const Home = React.createClass({
   render() {
     return (
       <div>
-        <p>
-          The url `/pictures/:id` can be rendered anywhere in the app as a modal.
-          Simply put `modal: true` in the `state` prop of links.
-        </p>
-
-        <p>
-          Click on an item and see its rendered as a modal, then copy/paste the
-          url into a different browser window (with a different session, like
-          Chrome -> Firefox), and see that the image does not render inside the
-          overlay. One URL, two session dependent screens :D
-        </p>
-
         <div>
-          {PICTURES.map(picture => (
-            <Link key={picture.id} to={`/pictures/${picture.id}`} state={{ modal: true, returnTo: this.props.location.pathname }}>
-              <img style={{ margin: 10 }} src={picture.src} height="100" />
+          {Cats.map(cat => (
+            <Link key={cat.cid} to={`/pictures/${cat.cid}`} state={{ modal: true, returnTo: this.props.location.pathname }}>
+              <img style={{ margin: 10 }} src={cat.get('src')} height="100" />
             </Link>
           ))}
         </div>
@@ -118,10 +55,10 @@ const Deep = React.createClass({
         <p>You can link from anywhere really deep too</p>
         <p>Params stick around: {this.props.params.one} {this.props.params.two}</p>
         <p>
-          <Link to={`/pictures/0`} state={{ modal: true, returnTo: this.props.location.pathname }}>
+          <Link to={`/pictures/c0`} state={{ modal: true, returnTo: this.props.location.pathname }}>
             Link to picture with Modal
           </Link><br/>
-          <Link to={`/pictures/0`}>
+          <Link to={`/pictures/c0`}>
             Without modal
           </Link>
         </p>
@@ -134,7 +71,7 @@ const Picture = React.createClass({
   render() {
     return (
       <div>
-        <img src={PICTURES[this.props.params.id].src} style={{ height: '80%' }} />
+        <img src={Cats.get(this.props.params.id).get('src')} style={{ height: '80%' }} />
       </div>
     )
   }
@@ -143,7 +80,7 @@ const Picture = React.createClass({
 render((
   <Router history={history}>
     <Route path="/" component={App}>
-      <IndexRoute component={Index}/>
+      <IndexRoute component={Home}/>
       <Route path="/pictures/:id" component={Picture}/>
       <Route path="/some/:one/deep/:two/route" component={Deep}/>
     </Route>
