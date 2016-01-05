c222251e639f3bf94485074847fd63a52f3db81d
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-05 22:25:49 +0530

    [Chapter 8] Make `Perf` available on window

--- chapter8/src/index.js
+++ chapter8/src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import Perf from 'react-addons-perf';
+
+window.Perf = Perf;
 
 ReactDOM.render(<App />, document.getElementById('rootElement'));
