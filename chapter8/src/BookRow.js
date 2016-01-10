bd41b149cd754f9e926f527a44b3516288d4857e
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-10 19:32:24 +0530

    Example of shouldComponentUpdate for BookRow component

--- chapter8/src/BookRow.js
+++ chapter8/src/BookRow.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.title !== this.props.title ||
+           nextProps.author_name !== this.props.author_name ||
+           nextProps.edition_count !== this.props.edition_count;
+  },
+
   render() {
     return(
       <tr style={this.props.style}>
