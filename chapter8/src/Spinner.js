30dd3b875fc354d0280ceb6f4ba68c6e463cc6f3
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-10 18:15:31 +0530

    Converted Spinner into ES6 style component

--- chapter8/src/Spinner.js
+++ chapter8/src/Spinner.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-var Spinner = React.createClass({
+export default React.createClass({
   render() {
     return(
       <div className="row">
-        <div className="col-lg-8 col-lg-offset-2">
-          <div className='text-center'><i className="fa fa-spinner fa-pulse fa-5x"></i></div>
+        <div className='text-center'>
+          <i className="fa fa-spinner fa-pulse fa-5x">
+          </i>
         </div>
       </div>
     );
   }
 });
-
-module.exports = Spinner;
